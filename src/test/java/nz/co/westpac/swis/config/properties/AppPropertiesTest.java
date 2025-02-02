9b0ae474979dfb4ba01e928bbefb3d61c87e684d
arorahrsh <nullpointerexception07@gmail.com>
2025-02-02 14:45:30 +1300

    Remove unnecessary annotation

--- src/test/java/nz/co/westpac/swis/config/properties/AppPropertiesTest.java
+++ src/test/java/nz/co/westpac/swis/config/properties/AppPropertiesTest.java
@@ -3,12 +3,9 @@ package nz.co.westpac.swis.config.properties;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AppPropertiesTest {
 
